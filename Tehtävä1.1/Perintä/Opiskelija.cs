69248b5e4405985410263f4e97817780111524bb
Matilda <Matilda@DESKTOP-DHTIFC9>
2022-01-11 10:53:31 +0200

    Perintä Esimerkki

---
+++ Tehtävä1.1/Perintä/Opiskelija.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perintä
+{
+    class Opiskelija : Henkilö
+    {
+        public int opiskelijaID;
+        public string koulu;
+
+        public Opiskelija(string nimi, int ikä, int henkilötunnus, int _opiskelijaID, string _koulu) : base(nimi, ikä, henkilötunnus)
+        {
+            opiskelijaID = _opiskelijaID;
+            koulu = _koulu;
+        }
+
+        public override void TulostaTiedot()
+        {
+            Console.WriteLine("Opiskelija: " + Nimi + ". Ikä: " + Ikä + ". Koulu: " + koulu + ". Opiskelija ID: " + opiskelijaID);
+        }
+
+        public void Opiskele()
+        {
+            Console.WriteLine(Nimi + " opiskelee...");
+        }
+    }
+}
---
+++ Tehtävä1.1/Perintä/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Perintä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Henkilö henkilö = new Henkilö("Matti", 20, 1042104);
+            Opiskelija opiskelija = new Opiskelija("Pekka", 18, 2155821, 12345, "Careeria");
+
+            henkilö.TulostaTiedot();
+            opiskelija.TulostaTiedot();
+
+            henkilö.Syö();
+            henkilö.Juo();
+            opiskelija.Syö();
+            opiskelija.Juo();
+            opiskelija.Opiskele();
+        }
+    }
+}
