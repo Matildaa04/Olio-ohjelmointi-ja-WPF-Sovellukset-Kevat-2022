3fc9e864c412e37b41063af59a6d64f6a8769b91
Matilda <Matilda@DESKTOP-DHTIFC9>
2022-01-11 13:31:27 +0200

    Harjoitus7

---
+++ Tehtävä1.1/Harjoitus8(KT)/Pomo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus8_KT_
+{
+    class Pomo : Henkilö
+    {
+    }
+}
---
+++ Tehtävä1.1/Harjoitus8(KT)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Harjoitus8_KT_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
