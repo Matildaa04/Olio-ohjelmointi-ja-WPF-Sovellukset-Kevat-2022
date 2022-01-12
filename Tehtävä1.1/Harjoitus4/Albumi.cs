81b7fa01d5f9fba274043225cd747231ab83f727
Matilda <Matilda@DESKTOP-DHTIFC9>
2022-01-12 21:34:33 +0200

    Harjoitus4

--- Tehtävä1.1/Harjoitus4/Albumi.cs
+++ Tehtävä1.1/Harjoitus4/Albumi.cs
@@ -6,21 +6,45 @@ namespace Harjoitus4
 {
     class Albumi
     {
+
+
         private List<Kappale> kappaleet = new List<Kappale>();
-
-
 
         public void LisääKappale(Kappale uusiKappale)
         {
             kappaleet.Add(uusiKappale);
         }
 
+        // Tulostaa albumissa olevat kappaleet
         public void TulostaKappaleet()
         {
+            
             foreach (Kappale kappale in kappaleet)
             {
                 Console.WriteLine(kappale.Nimi + ", " + kappale.Pituus);
             }
         }
+
+        public string Artisti;
+        public string Nimi;
+        public string Genre;
+        public int Hinta;
+
+        public Albumi(string artisti, string nimi, string genre, int hinta)
+        {
+            Artisti = artisti;
+            Nimi = nimi;
+            Genre = genre;
+            Hinta = hinta;
+        }
+
+        public void TulostaAlbumi()
+        {
+            Console.WriteLine("Artisti: " + Artisti);
+            Console.WriteLine("Albumin nimi: " + Nimi);
+            Console.WriteLine("Genre: " + Genre);
+            Console.WriteLine("Hinta: " + Hinta);
+        }
+
     }
 }
--- Tehtävä1.1/Harjoitus4/Program.cs
+++ Tehtävä1.1/Harjoitus4/Program.cs
@@ -6,17 +6,27 @@ namespace Harjoitus4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Albumi: ");
+            Console.WriteLine("-----------------------------");
+            Albumi albumi = new Albumi("David Bowie","Scary Monsters","pop/rock",10);
 
+            albumi.TulostaAlbumi();
 
-            Albumi albumi = new Albumi();
-
-            Console.WriteLine("songs: ");
-            albumi.LisääKappale(new Kappale("Kappale 1", "06:25"));
-            albumi.LisääKappale(new Kappale("Kappale 2", "02:55"));
-            albumi.LisääKappale(new Kappale("Kappale 3", "05:12"));
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Biisit: ");
+            Console.WriteLine("-----------------------------");
+            albumi.LisääKappale(new Kappale("It's No Game", "04:16"));
+            albumi.LisääKappale(new Kappale("Up The Hill Backwards","03:15"));
+            albumi.LisääKappale(new Kappale("Scary Monsters","05:13"));
+            albumi.LisääKappale(new Kappale("Ashes to Ashes","04:25"));
+            albumi.LisääKappale(new Kappale("Fashion","04:49"));
+            albumi.LisääKappale(new Kappale("Teenage Wildlife","06:56"));
+            albumi.LisääKappale(new Kappale("Scream Like a Baby","03:36"));
+            albumi.LisääKappale(new Kappale("Kingdom Come","03:47"));
+            albumi.LisääKappale(new Kappale("Because You're Young","04:55"));
 
             albumi.TulostaKappaleet();
-
         }
     }
 }
