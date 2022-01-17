5939db258e6db4fe5e65359a1c998a381805ecc6
Matilda <Matilda@DESKTOP-DHTIFC9>
2022-01-17 09:54:22 +0200

    Harjoitus10

---
+++ Tehtävä1.1/Harjoitus10/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus10
+{
+    class Animal
+    {
+
+        public virtual void MakeASound()
+        {
+            Console.WriteLine("Animal makes sound");
+        }
+    }
+}
---
+++ Tehtävä1.1/Harjoitus10/Sorsa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus10
+{
+    class Sorsa : Animal, ICanFly, ICanSwim
+    {
+
+        public float WingSize { get; set; }
+        public float SwimSpeed { get; set; }
+
+        public Sorsa(float wingSize, float swimSpeed)
+        {
+            WingSize = wingSize;
+            SwimSpeed = swimSpeed;
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Sorsa lentää. Sorsan siipien koko on " + WingSize);
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Sorsa ui. Sorsan uintinopeus on " + SwimSpeed);
+        }
+
+        public override void MakeASound()
+        {
+            Console.WriteLine("Quack quack");
+        }
+    }
+}
