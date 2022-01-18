59684f1d22c139ed0d1ea7843cb262091b43d3dc
Matilda <Matilda@DESKTOP-DHTIFC9>
2022-01-18 12:29:24 +0200

    Harjoitus17

---
+++ Tehtävä1.1/Harjoitus17/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harjoitus17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        float lämpötila = 20;
+        bool keittiöValot = false;
+        bool olohuoneenValot = false;
+        bool oviLukossa = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tb_Lämpötila.Text = "Talon sisälämpötila on " + lämpötila;
+            tb_KeittiöTila.Text = keittiöValot.ToString();
+            tb_OlohuoneTila.Text = olohuoneenValot.ToString();
+            btn_Ovi.Content = oviLukossa.ToString();
+        }
+
+        private void btn_Olohuone_Click(object sender, RoutedEventArgs e)
+        {
+            olohuoneenValot = !olohuoneenValot;
+            tb_OlohuoneTila.Text = olohuoneenValot.ToString();
+        }
+
+        private void btn_Keittiö_Click(object sender, RoutedEventArgs e)
+        {
+            keittiöValot = !keittiöValot;
+            tb_OlohuoneTila.Text = olohuoneenValot.ToString();
+        }
+
+        private void btn_Ovi_Click(object sender, RoutedEventArgs e)
+        {
+            oviLukossa = !oviLukossa;
+            btn_Ovi.Content = oviLukossa.ToString();
+        }
+
+        private void txt_Termostaatti_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           lämpötila = int.Parse(txt_Termostaatti.Text);
+            tb_Lämpötila.Text = "Talon sisälämpötila on " + lämpötila;
+        }
+    }
+}
